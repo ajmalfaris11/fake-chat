dc3ddbe5c90e0ba896fff4d3ce92782cdacb44d7
ajmal faris <ajmalfaris.me@gmail.com>
2025-07-17 17:51:40 +0530

    carousel code structured

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,25 +1,27 @@
+import React from "react";
 import { Carousel } from "@material-tailwind/react";
 
 import buildTrust from "../assets/Carousel/buildTrust.webp";
 import increaseSales from "../assets/Carousel/increaseSales.webp";
 
-export function CarouselCustomNavigation() {
+export default function CarouselCustomNavigation() {
   return (
     <Carousel
       className="top-2 rounded-2xl"
-      autoplay={true}
+      autoplay
       autoplayDelay={5000}
-      loop={true}
+      loop
       prevArrow={() => null}
       nextArrow={() => null}
       navigation={({ setActiveIndex, activeIndex, length }) => (
-        <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
-          {new Array(length).fill("").map((_, i) => (
+        <div className="absolute bottom-4 left-1/2 z-50 flex -translate-x-1/2 gap-2">
+          {Array.from({ length }).map((_, i) => (
             <span
               key={i}
-              className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${activeIndex === i ? "w-8 bg-white" : "w-4 bg-white/50"
-                }`}
               onClick={() => setActiveIndex(i)}
+              className={`block h-1 cursor-pointer rounded-2xl transition-all ${
+                activeIndex === i ? "w-8 bg-white" : "w-4 bg-white/50"
+              }`}
             />
           ))}
         </div>
@@ -27,13 +29,12 @@ export function CarouselCustomNavigation() {
     >
       <img
         src={buildTrust}
-        alt="image 1"
+        alt="Build Trust"
         className="w-full object-cover"
       />
-
       <img
         src={increaseSales}
-        alt="image 2"
+        alt="Increase Sales"
         className="w-full object-cover"
       />
     </Carousel>
