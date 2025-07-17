1f3eac6bab1e5891e2d417aa7b78fe14e82e4779
ajmal faris <ajmalfaris.me@gmail.com>
2025-07-17 17:55:08 +0530

    feat: integrate React Router and setup HomePage route

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
